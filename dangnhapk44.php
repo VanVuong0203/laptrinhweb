b213c3106f6cfa934d733f69fd1555e7d9b26cee
Huỳnh Văn Vương <126145490+VanVuong0203@users.noreply.github.com>
2023-02-24 08:07:23 +0700

    Add files via upload

---
+++ dangnhapk44.php
@@ -0,0 +1,17 @@
+<html>
+    <meta charset="utf8">
+<head><title>
+        Lập Trình Web</title>
+</head>
+<body>
+    <table align="center" border="0">
+<form action="#"method="POST">
+    <tr><td align="center" colspan="2"><b>Đăng Nhập</b></td></tr>
+    <tr><td>Tên tài khoản:</td><td><input type="Text" name="tentk"></td></tr>
+    <tr><td>Mật khẩu:</td><td><input type="PassWord" name="pass"></td></tr>
+    <tr><td colspan="2" align="left"><input type="checkbox" name="mk" value="nhớ mật khẩu"><b>Nhớ mật khẩu</b></td></tr>
+    <tr><td colspan="2" align="right"><input type="submit" value="Đăng Nhập" name="dn"></td></tr>
+</form>
+</table>
+</body>
+</html>

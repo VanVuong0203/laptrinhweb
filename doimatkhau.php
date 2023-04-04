8960400674f530bf2fd0e58c7b68cebbfca0df15
Huỳnh Văn Vương <126145490+VanVuong0203@users.noreply.github.com>
2023-04-04 16:19:38 +0700

    Add files via upload

---
+++ doimatkhau.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>Thay đổi mật khẩu</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+   <form action = "" method= "POST">
+   <table align="center" border="0" cellpadding="3" cellspacing="0.5" style="background-color:LightGray;">
+        <tr class= "tr1">
+        <tr>
+        <td align="center" colspan="2" style="background-color:Gray;" ><b>Thay Đổi Mật Khẩu</b></td>
+        </tr>
+			<tr>
+			    <td>Đổi mật khẩu cho:</td>
+                <td></td>
+		    </tr>
+			<tr>
+			    <td>Mật khẩu cũ:</td>
+				<td><input type="text" placeholder="Mat khau cu"></td>
+			</tr>
+			<tr>
+			    <td> Mật khẩu mới:</td>
+				<td><input type="password" placeholder="mat khau moi"></td>
+			</tr>
+            <tr>
+			    <td> Nhập lại mật khẩu:</td>
+				<td><input type="password" placeholder="mat khau moi"></td>
+			</tr>
+            <tr>
+                <td colspan="2" align="right"><input type="submit" value="Thay Đổi" name="td" style="background-color:LightBlue;"></td>
+            </tr>
+	</table>
+	</form>
+    </body>
+</html>
